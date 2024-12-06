04fabe241e93310dca00db46a87bac111db39f24
FoleyLab <jayfoleyiv@gmail.com>
2024-12-06 10:04:36 -0500

    updated

--- src/examples/H2_dimers/pyscf_dir/H2_CASSCF_Monomer.py
+++ src/examples/H2_dimers/pyscf_dir/H2_CASSCF_Monomer.py
@@ -3,10 +3,8 @@ from pyscf import gto, scf, mp, mcscf
 
 mol = gto.M(
     atom = '''
-    H 0.0000000000   0.0000000000  11999.62869866
-    H 0.0000000000   0.0000000000  12000.37130134
-    H 0.0000000000   0.0000000000 -12000.37130134
-    H 0.0000000000   0.0000000000 -11999.62869866
+         H            0.000000000000     0.000000000000    -0.371301335400
+         H            0.000000000000     0.000000000000     0.371301335400    
     ''',
     basis = 'cc-pvtz',
     spin = 0,
@@ -18,10 +16,9 @@ mf = scf.RHF(mol)
 ehf = mf.kernel()
 #noons, natorbs = mcscf.addons.make_natural_orbitals(mymp)
 #mycas = mcscf.CASCI(myhf, ncas, nelecas)
-n_states = 6
-weights = np.array([1, 1, 1, 0, 0, 1]) / 4
-#np.ones(n_states)/4
-mc = mcscf.CASSCF(mf, 8, 4).state_average_(weights)
+n_states = 2
+weights = np.ones(n_states)/n_states
+mc = mcscf.CASSCF(mf, 2, 2).state_average_(weights)
 mc.fix_spin_(ss=0)
 mc.conv_tol = 1e-12
 mc.kernel()
