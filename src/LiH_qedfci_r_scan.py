e347a0c04ac87e09adf226cc2cfb911bb12fcebf
FoleyLab <jayfoleyiv@gmail.com>
2025-01-17 13:44:11 -0500

    fixed some misnamed dictionary

--- src/LiH_qedfci_r_scan.py
+++ src/LiH_qedfci_r_scan.py
@@ -4,8 +4,8 @@ sys.path.append("/home/nvu12/software/qed_ci_main/qed_ci_casscf4/qed-ci/src/")
 
 # import helper libraries
 import numpy as np
-import psi4
-from helper_PFCI import PFHamiltonianGenerator
+#import psi4
+#from helper_PFCI import PFHamiltonianGenerator
 import json 
 
 # set precision for printing numpy arrays
@@ -15,7 +15,7 @@ np.set_printoptions(threshold=sys.maxsize)
 file_string = "LiH_r_scan_6311g_qedfci"
 
 # create template for molecular geometry
-mol_str = """
+mol_tmpl = """
 Li
 H 1 **R**
 symmetry c1
@@ -32,7 +32,7 @@ options_dict = {
 # specify input parameters that are unique to the cavity quantum electrodynamics calculation
 cavity_options = {
     'omega_value' : 0.12086,
-    'lambda_vector' : np.array([0, 0, 0.05]),
+    'lambda_vector' : [0, 0, 0.05],
     'ci_level' : 'cas',
     'ignore_coupling' : False,
     'number_of_photons' : 1,
@@ -67,7 +67,7 @@ calculation_data = {
 
 }
 # store cavity parameters in dictionary
-for keys, values in cavity_dict.items():
+for keys, values in cavity_options.items():
     calculation_data[keys] = values
 # store standard electronic structure data in dictionary
 for keys, values in options_dict.items():
@@ -80,56 +80,26 @@ N_r_values = 10
 # create a list of r values between 1.0 and 3.0 angstroms
 r_values = np.linspace(1.0, 3.0, N_r_values)
 
-test_pf = PFHamiltonianGenerator(
-        mol_str,
-        options_dict,
-        cavity_dict
-)
-
-# Sample data structure representing TD-DFT calculation results
-data = {
-    "basis_set": "6-31G*",
-    "functional": "B3LYP",
-    "convergence_threshold": 1e-6,
-    "points": [
-        {
-            "geometry": [
-                {"atom": "H", "x": 0.0, "y": 0.0, "z": 0.0},
-                {"atom": "O", "x": 0.0, "y": 0.0, "z": 0.96},
-                {"atom": "H", "x": 0.0, "y": 0.75, "z": -0.48}
-            ],
-            "ground_state_energy": -76.345,
-            "excited_state_energies": [-76.234, -76.200],
-            "ground_state_dipole_moment": [0.0, 0.0, 1.85],
-            "excited_state_dipole_moments": [
-                [0.1, 0.0, 1.9],
-                [0.0, 0.2, 1.8]
-            ],
-            "transition_dipole_moments": [
-                [0.05, 0.0, 0.1],
-                [0.02, 0.1, 0.08]
-            ]
-        },
-        {
-            "geometry": [
-                {"atom": "H", "x": 0.0, "y": 0.0, "z": 0.0},
-                {"atom": "O", "x": 0.0, "y": 0.0, "z": 1.0},
-                {"atom": "H", "x": 0.0, "y": 0.8, "z": -0.5}
-            ],
-            "ground_state_energy": -76.342,
-            "excited_state_energies": [-76.230, -76.198],
-            "ground_state_dipole_moment": [0.0, 0.0, 1.83],
-            "excited_state_dipole_moments": [
-                [0.1, 0.0, 1.88],
-                [0.0, 0.2, 1.78]
-            ],
-            "transition_dipole_moments": [
-                [0.05, 0.0, 0.09],
-                [0.02, 0.1, 0.07]
-            ]
-        }
-    ]
-}
+for r_val in r_values:
+    # update the molecular geometry with the new r value
+    mol_str = mol_tmpl.replace("**R**", str(r_val))
+    
+    # create a new instance of the PFHamiltonianGenerator class
+    #test_pf = PFHamiltonianGenerator(
+    #    mol_str,
+    #    options_dict,
+    #    cavity_options)
+    # create random energy values for 3 different states
+    energy_values = [-100., -102., -113.]
+    # create random dipole values for 3 different states
+    dipole_values = [-5., -6., -7.]
+    # add mol_str to the molecular geometry dictionary
+    calculation_data["molecular_geometry"]["z-matrix"].append(mol_str)
+    # add energy values to the return_results dictionary
+    calculation_data["return_results"]["energies"].append(energy_values)
+    # add dipole moments to the return_results dictionary
+    calculation_data["return_results"]["dipoles"].append(dipole_values)
+    
 
 # Function to write data to JSON file
 def write_to_json(data, filename):
@@ -137,8 +107,8 @@ def write_to_json(data, filename):
         json.dump(data, json_file, indent=4)
 
 # Writing data to a JSON file
-output_filename = "td_dft_pes_data.json"
-write_to_json(data, output_filename)
+output_filename = "new_data.json"
+write_to_json(calculation_data, output_filename)
 
 print(f"Data successfully written to {output_filename}")
 
